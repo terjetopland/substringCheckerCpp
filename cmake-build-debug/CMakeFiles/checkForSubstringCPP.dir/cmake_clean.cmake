ef656a9549caa53cf27ee232bcc52a8a4b088be2
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2021-12-04 10:51:55 +0100

    just trying1

---
+++ cmake-build-debug/CMakeFiles/checkForSubstringCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkForSubstringCPP.dir/main.cpp.obj"
+  "checkForSubstringCPP.exe"
+  "checkForSubstringCPP.exe.manifest"
+  "checkForSubstringCPP.pdb"
+  "libcheckForSubstringCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checkForSubstringCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void greet() {
+    cout << "Hello World";
+}
+int main() {
+    cout << "Hello, World!" << endl;
+
+    greet();
+    return 0;
+}
